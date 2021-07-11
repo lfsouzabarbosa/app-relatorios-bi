5be1e065d66bd1a05b7cc620f5f46d69c083f7b6
Luiz Fernando <lfsouzabarbosa@gmail.com>
2021-07-11 20:24:42 -0300

    relatorio BI previsao de londres

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send('<iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiODYyMWU0YTYtYjM5NC00ZjM0LWE4MWUtODZkMzFiMDQ4Zjg5IiwidCI6Ijg3ZTcxOWVhLTg0MGUtNDFiMy05Y2NkLTM5MDg2YzllY2NiNiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>');
+    res.send('<iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYTZlNWJiZmEtMjdhYy00YmUzLTg1ZTctMTcyMzk2Y2ZjYjk2IiwidCI6Ijg3ZTcxOWVhLTg0MGUtNDFiMy05Y2NkLTM5MDg2YzllY2NiNiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>');
 });
 
 app.listen(PORT, HOST);
