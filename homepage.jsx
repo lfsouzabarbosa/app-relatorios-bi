f2127d3f6c011cce0ca48ae8797d1fb1b4a7aa49
Luiz Fernando <lfsouzabarbosa@gmail.com>
2021-07-28 14:57:14 -0300

    report c6 news

--- homepage.jsx
+++ homepage.jsx
@@ -76,6 +76,11 @@ const Dashboard = () => {
             </section>
           </section>
         </section>
+
+      <h5 font-size="xl" class="sc-dIvqjp lbrNjM sc-fKgIGh admin-bro_Header admin-bro_H5" font-weight="normal">Google News - Power BI</h5>
+      <br></br>
+      <br></br>
+      <iframe width="1000" height="500" src="https://app.powerbi.com/view?r=eyJrIjoiZWFhODZkNzItZDc0YS00YTJlLThmMmEtYzk1OTY0MDg0MzY1IiwidCI6Ijg3ZTcxOWVhLTg0MGUtNDFiMy05Y2NkLTM5MDg2YzllY2NiNiIsImMiOjR9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
     </Box>
   )
 }
