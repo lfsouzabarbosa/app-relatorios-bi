9043fc5ad16a443c310d3f9766815c53b9f6e955
Luiz Fernando <lfsouzabarbosa@gmail.com>
2021-09-10 00:04:58 -0300

    dados visiveis em producao

--- api.js
+++ api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const apiSheets = axios.create({
      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   // baseURL: "https://viacep.com.br/ws/01001000/json/",
-  //   baseURL: "http//vps23593.publiccloud.com.br:3009/",
-       baseURL: "http://localhost:3009/",
+     baseURL: "http://vps23593.publiccloud.com.br:3009/",
+  //     baseURL: "http://localhost:3009/",
 
 });
 
