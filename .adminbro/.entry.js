db9a8a7dbc39dc23063485664094f04f923ecfbb
Luiz Fernando <lfsouzabarbosa@gmail.com>
2021-11-03 13:06:49 -0300

    grafico de interesse

--- .adminbro/.entry.js
+++ .adminbro/.entry.js
@@ -1,39 +1,13 @@
 AdminBro.UserComponents = {}
 import Component1 from '../c6Bank'
 AdminBro.UserComponents.Component1 = Component1
-import Component2 from '../c6Bank'
+import Component2 from '../suhai'
 AdminBro.UserComponents.Component2 = Component2
-import Component3 from '../homepage'
+import Component3 from '../suhaidaily'
 AdminBro.UserComponents.Component3 = Component3
-import Component4 from '../tumelero'
+import Component4 from '../suhaiReport7dias'
 AdminBro.UserComponents.Component4 = Component4
-import Component5 from '../obraja'
+import Component5 from '../suhaiReport24horas'
 AdminBro.UserComponents.Component5 = Component5
-import Component6 from '../klabin'
+import Component6 from '../suhaidaily'
 AdminBro.UserComponents.Component6 = Component6
-import Component7 from '../locaweb'
-AdminBro.UserComponents.Component7 = Component7
-import Component8 from '../mitsubishi'
-AdminBro.UserComponents.Component8 = Component8
-import Component9 from '../suhai'
-AdminBro.UserComponents.Component9 = Component9
-import Component10 from '../suzuki'
-AdminBro.UserComponents.Component10 = Component10
-import Component11 from '../c6Bank'
-AdminBro.UserComponents.Component11 = Component11
-import Component12 from '../homepage'
-AdminBro.UserComponents.Component12 = Component12
-import Component13 from '../tumelero'
-AdminBro.UserComponents.Component13 = Component13
-import Component14 from '../obraja'
-AdminBro.UserComponents.Component14 = Component14
-import Component15 from '../klabin'
-AdminBro.UserComponents.Component15 = Component15
-import Component16 from '../locaweb'
-AdminBro.UserComponents.Component16 = Component16
-import Component17 from '../mitsubishi'
-AdminBro.UserComponents.Component17 = Component17
-import Component18 from '../suhai'
-AdminBro.UserComponents.Component18 = Component18
-import Component19 from '../suzuki'
-AdminBro.UserComponents.Component19 = Component19
