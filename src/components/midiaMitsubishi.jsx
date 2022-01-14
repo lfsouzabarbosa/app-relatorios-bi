db55f2c8f85449c905af92768977325be5a246a7
Ellen Tavares <ellentavaresp@gmail.com>
2022-01-14 13:07:20 -0300

    novo arquivo midiaMitsubishi

---
+++ src/components/midiaMitsubishi.jsx
@@ -0,0 +1,29 @@
+import { ApiClient } from 'admin-bro'
+import { Box } from '@admin-bro/design-system'
+import { Text } from '@admin-bro/design-system'
+import { Button } from '@admin-bro/design-system'
+import { Link } from '@admin-bro/design-system'
+import { UserFollow32, FavoriteFilled32, SendAltFilled32 } from '@carbon/icons-react'
+import styled from 'styled-components'
+import {
+    Table,
+    TableRow,
+    TableCell,
+    TableCaption,
+    TableHead,
+    TableBody,
+} from '@admin-bro/design-system'
+
+const api = new ApiClient()
+
+const Dashboard = () => {
+
+    return ( 
+        <Text>oi
+            
+        </Text>
+    )
+};
+
+export default Dashboard
+
