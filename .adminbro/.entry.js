c30aaa4c02da90e8bd8aa6f2fe42fabd4c3ae00e
Luiz Fernando <lfsouzabarbosa@gmail.com>
2022-01-14 13:17:03 -0300

    pagina de midia da mitisubish

--- .adminbro/.entry.js
+++ .adminbro/.entry.js
@@ -15,15 +15,71 @@ import Component7 from '../src/components/locaweb'
 AdminBro.UserComponents.Component7 = Component7
 import Component8 from '../src/components/mitsubishi'
 AdminBro.UserComponents.Component8 = Component8
-import Component9 from '../src/components/mitsubishi'
+import Component9 from '../src/components/c6Bank'
 AdminBro.UserComponents.Component9 = Component9
-import Component10 from '../src/components/suhai'
+import Component10 from '../src/components/homepage'
 AdminBro.UserComponents.Component10 = Component10
-import Component11 from '../src/components/suhaiFlashReport'
+import Component11 from '../src/components/tumelero'
 AdminBro.UserComponents.Component11 = Component11
-import Component12 from '../src/components/suzuki'
+import Component12 from '../src/components/obraja'
 AdminBro.UserComponents.Component12 = Component12
-import Component13 from '../src/components/InvestimentosMidia'
+import Component13 from '../src/components/klabin'
 AdminBro.UserComponents.Component13 = Component13
-import Component14 from '../src/components/midia'
+import Component14 from '../src/components/locaweb'
 AdminBro.UserComponents.Component14 = Component14
+import Component15 from '../src/components/mitsubishi'
+AdminBro.UserComponents.Component15 = Component15
+import Component16 from '../src/components/c6Bank'
+AdminBro.UserComponents.Component16 = Component16
+import Component17 from '../src/components/homepage'
+AdminBro.UserComponents.Component17 = Component17
+import Component18 from '../src/components/tumelero'
+AdminBro.UserComponents.Component18 = Component18
+import Component19 from '../src/components/obraja'
+AdminBro.UserComponents.Component19 = Component19
+import Component20 from '../src/components/klabin'
+AdminBro.UserComponents.Component20 = Component20
+import Component21 from '../src/components/locaweb'
+AdminBro.UserComponents.Component21 = Component21
+import Component22 from '../src/components/mitsubishi'
+AdminBro.UserComponents.Component22 = Component22
+import Component23 from '../src/components/c6Bank'
+AdminBro.UserComponents.Component23 = Component23
+import Component24 from '../src/components/homepage'
+AdminBro.UserComponents.Component24 = Component24
+import Component25 from '../src/components/tumelero'
+AdminBro.UserComponents.Component25 = Component25
+import Component26 from '../src/components/obraja'
+AdminBro.UserComponents.Component26 = Component26
+import Component27 from '../src/components/klabin'
+AdminBro.UserComponents.Component27 = Component27
+import Component28 from '../src/components/locaweb'
+AdminBro.UserComponents.Component28 = Component28
+import Component29 from '../src/components/mitsubishi'
+AdminBro.UserComponents.Component29 = Component29
+import Component30 from '../src/components/c6Bank'
+AdminBro.UserComponents.Component30 = Component30
+import Component31 from '../src/components/homepage'
+AdminBro.UserComponents.Component31 = Component31
+import Component32 from '../src/components/tumelero'
+AdminBro.UserComponents.Component32 = Component32
+import Component33 from '../src/components/obraja'
+AdminBro.UserComponents.Component33 = Component33
+import Component34 from '../src/components/klabin'
+AdminBro.UserComponents.Component34 = Component34
+import Component35 from '../src/components/locaweb'
+AdminBro.UserComponents.Component35 = Component35
+import Component36 from '../src/components/mitsubishi'
+AdminBro.UserComponents.Component36 = Component36
+import Component37 from '../src/components/midiaMitsubish'
+AdminBro.UserComponents.Component37 = Component37
+import Component38 from '../src/components/suhai'
+AdminBro.UserComponents.Component38 = Component38
+import Component39 from '../src/components/suhaiFlashReport'
+AdminBro.UserComponents.Component39 = Component39
+import Component40 from '../src/components/suzuki'
+AdminBro.UserComponents.Component40 = Component40
+import Component41 from '../src/components/InvestimentosMidia'
+AdminBro.UserComponents.Component41 = Component41
+import Component42 from '../src/components/midia'
+AdminBro.UserComponents.Component42 = Component42
